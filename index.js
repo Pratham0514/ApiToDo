500bbeeea79f3a4f0942352191c1f33207deaba1
Pratham0514 <prathameshroundale05@gmail.com>
2026-03-04 13:45:34 +0530

    core set

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createRef } from "react";
+import cors from "cors";
 import dotenv from "dotenv";
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 dotenv.config();
 
 const TODO_ITEMS = [
